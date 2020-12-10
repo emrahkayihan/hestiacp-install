445c8184d1b39ef28dcd046fb6bb74b9ff93efa9
emrahkayihan <emrahkayihan@gmail.com>
2020-12-10 12:43:00 +0000

    Update hst-install-ubuntu.sh

--- hst-install-ubuntu.sh
+++ hst-install-ubuntu.sh
@@ -33,9 +33,7 @@ mariadb_v="10.5"
 software="apache2 apache2.2-common apache2-suexec-custom apache2-utils nginx apparmor-utils awstats bc bsdmainutils bsdutils cron curl dnsutils e2fslibs e2fsprogs expect fail2ban flex ftp git idn imagemagick
     libapache2-mod-fcgid libapache2-mod-php$fpm_v libapache2-mod-rpaf
     lsof mc mariadb-client mariadb-common mariadb-server php$fpm_v php$fpm_v-cgi php$fpm_v-common php$fpm_v-curl phpmyadmin
-    php$fpm_v php$fpm_v-cgi php$fpm_v-common php$fpm_v-curl phpmyadmin 
-    php$fpm_v-mysql php$fpm_v-imap php$fpm_v-ldap php$fpm_v-apcu phppgadmin
-    php$fpm_v-pgsql php$fpm_v-zip php$fpm_v-bz2 php$fpm_v-cli php$fpm_v-gd
+    php$fpm_v php$fpm_v-cgi php$fpm_v-common php$fpm_v-curl php$fpm_v-mysql php$fpm_v-imap php$fpm_v-ldap php$fpm_v-apcu php$fpm_v-pgsql php$fpm_v-zip php$fpm_v-bz2 php$fpm_v-cli php$fpm_v-gd
     php$fpm_v-imagick php$fpm_v-intl php$fpm_v-json php$fpm_v-mbstring
     php$fpm_v-opcache php$fpm_v-pspell php$fpm_v-readline php$fpm_v-xml 
     quota rrdtool rssh sudo hestia=${HESTIA_INSTALL_VER}
